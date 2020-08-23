ba37ea19a8855ed50f68fc35299901293121cb71
Francois PINOT <fggpinot@gmail.com>
2020-08-23 11:16:18 +0200

    Fixed dead link to mkdb in quickref

--- quickref-fr.py
+++ quickref-fr.py
@@ -108,6 +108,10 @@ except:
 files[len(files):]=glob.glob('opcodes/*/*.xml')
 files[len(files):]=glob.glob('vectorial/*.xml')
 files[len(files):]=glob.glob('utility/*.xml')
+try:
+    files.remove('utility/mkdb.xml')
+except:
+    pass
 files.sort()
 
 headerText = text[0:text.find('<book id="index"')]
@@ -236,4 +240,3 @@ for i in range(len(categories)):
 quickref.write('</chapter></part>\n')
 quickref.close()
 print(entries)
-
